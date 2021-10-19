2e90b9d0ee1d81a0305f841b76b60a5d288c0b3c
WendyWu <92006997+wendy61019@users.noreply.github.com>
2021-10-19 21:03:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ app.get('/', (req,res) => {
     res.render('index', { restaurants: restaurantList.results } )
 })
 
-app.get('/search', (req, res) => {
-    const keyword = req.query.keyword
-    const restaurants = restaurantList.results.filter(restaurant => {
-        return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-    })
-    res.render('index', { restaurants: restaurants, keyword: keyword })
-})
-
 app.get('/restaurants/:restaurant_id', (req, res) => {
     const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
     res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword.trim().toLowerCase()
+    const restaurants = restaurantList.results.filter(restaurant => {
+        return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.includes(keyword)
+    })
+    res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
 app.listen(port, () => {
