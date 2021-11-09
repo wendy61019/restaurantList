c58e186e377f59b79bf1c1f60a314e6a5d8ba5d7
WendyWu <92006997+wendy61019@users.noreply.github.com>
2021-11-09 11:48:43 +0800

    Update restaurantSeeder.js

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,20 +1,21 @@
-const Restaurant = require("../restaurant.js")
-const restaurantList = require("../../restaurant.json")
-
+const Restaurant = require('../restaurant')
+const restaurantList = require('../../restaurant.json').results
 const db = require('../../config/mongoose')
 
-db.once("open", () => {
-    restaurantList.results.forEach(data => {
-    const { 
-      name, category, image, location, 
-      phone, google_map, rating, description 
-    } = data
-
+db.once('open', () => {
+  restaurantList.forEach(restaurant => {
     Restaurant.create({
-      name, category, image, location,
-      phone, google_map, rating, description
+      name: restaurant.name,
+      name_en: restaurant.name_en,
+      category: restaurant.category,
+      image: restaurant.image,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      google_map: restaurant.google_map,
+      rating: restaurant.rating,
+      description: restaurant.description
     })
   })
-    
+
   console.log('restaurantSeeder done!')
 })
