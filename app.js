9b2dc03246a8d0b7f0f8f9963c7a8e2f37b56e9e
WendyWu <92006997+wendy61019@users.noreply.github.com>
2021-11-09 12:19:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
-const methodOverride = require("method-override")
-
-const routes = require('./routes')
-require('./config/mongoose')
+const methodOverride = require('method-override')
 const app = express()
+const routes = require('./routes')
 const port = 3000
+
+require('./config/mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -15,6 +13,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
 app.use(routes)
 
 app.listen(port, () => {
