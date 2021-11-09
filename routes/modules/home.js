e3e7a72d19fefc8887a09206d4f5b49c20c13a0b
WendyWu <92006997+wendy61019@users.noreply.github.com>
2021-11-09 12:01:40 +0800

    Update home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,55 +1,50 @@
 const express = require('express')
 const router = express.Router()
+
 const Restaurant = require('../../models/restaurant')
 
-//瀏覽全部餐廳
+// Index 頁面：瀏覽全部所有餐廳
 router.get('/', (req, res) => {
-    return Restaurant.find()
-        .lean()
-        .sort({ _id: 'asc' })
-        .then(restaurants => res.render('index', { restaurants }))
-        .catch(error => console.log(error))
+  console.log(req.query)
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
 })
 
-//搜尋特定餐廳 加入排序條件
+// 搜尋功能
 router.get('/search', (req, res) => {
+  const keywords = req.query.keyword.toLowerCase().trim()
+  const sortCriteria = req.query.sort
+  const sortMethod = {}
 
-    const sort = req.query.sort
-    const userInputKeyword = req.query.keyword.trim()
+  switch (sortCriteria) {
+    case 'asc':
+      sortMethod['name'] = 'asc'
+      break
+    case 'desc':
+      sortMethod['name'] = 'desc'
+      break
+    case 'category':
+      sortMethod['category'] = 'asc'
+      break
+    case 'location':
+      sortMethod['location'] = 'asc'
+      break
+    default:
+      sortMethod['name'] = 'asc'
+  }
 
-    const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/\\'
-    const processedKeyword = [...userInputKeyword].filter(arrayItem => !symbols.includes(arrayItem)).join('')
-
-    const regexKeyword= new RegExp(`${processedKeyword}`, 'i')
-    const sortCondition = {}
-    const sortOptions = [
-        { name:'A -> Z', value: 'asc' },
-        { name: 'Z -> A', value: 'desc' },
-        { name: '類別', value: 'category'},
-        { name: '地區', value: 'location' },
-    ]
-
-    sortOptions.forEach((item, index) => {
-        sortOptions[index]['isSelected'] = sort === item.value
+  Restaurant.find()
+    .lean()
+    .sort(sortMethod)
+    .then(restaurants => {
+      restaurants = restaurants.filter(restaurant => {
+        return restaurant.name.toLowerCase().includes(keywords) || restaurant.name_en.toLowerCase().includes(keywords) ||
+          restaurant.category.toLowerCase().includes(keywords)
+      })
+      res.render('index', { restaurants: restaurants, keywords: keywords })
     })
-
-    switch (true) {
-      case sort === 'asc'  ||  sort === 'desc':
-        sortCondition['name'] = sort
-        break
-      case sort === 'category' || sort === 'location':
-        sortCondition[sort] = 'asc'
-        break
-      default:
-        sortCondition['_id'] = 'asc'
-    }
-
-    Restaurant.find({ $or: [{ name: regexKeyword }, { category: regexKeyword }] })
-        .lean()
-        .sort(sortCondition)
-        .then(restaurants => res.render('index', { restaurants, keyword: userInputKeyword, sortOptions }
-        ))
-        .catch(error => console.log(error))
 })
 
 module.exports = router
