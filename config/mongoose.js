34461ba0bf8459dea2f631d4778ed9068cf35857
WendyWu <92006997+wendy61019@users.noreply.github.com>
2021-11-14 18:28:05 +0800

    feat: add heroku unit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/restaurant-list'
 
-mongoose.connect('mongodb://localhost/restaurant-list', {  
+mongoose.connect(MONGODB_URI, {  
     useNewUrlParser: true,
     useUnifiedTopology: true  })
 
