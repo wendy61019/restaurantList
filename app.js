7fb24abf9d2f86220b3b97c1c2d41927d967bd25
WendyWu <92006997+wendy61019@users.noreply.github.com>
2021-11-14 18:29:50 +0800

    feat: add heroku env

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const app = express()
 const routes = require('./routes')
-const port = 3000
+const port = process.env.PORT || 3000
 
 require('./config/mongoose')
 
